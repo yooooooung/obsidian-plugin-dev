9799c6369b061ebf31afbadcc548667028ff7ec1
gaojinman <gjm1998@foxmail.com>
2024-05-06 18:39:13 +0800

    main commit

--- main.ts
+++ main.ts
@@ -1,134 +1,109 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownView, Plugin, Editor, moment } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+export default class AutoNumberHeadingsPlugin extends Plugin {
+  onload() {
+    // 添加快捷键
+    this.addCommand({
+      id: "auto-number-headings",
+      name: "Auto Number Headings",
+	  hotkeys: [{ modifiers: ["Mod", "Shift"], key: "m" }],
+      editorCallback: (editor: Editor) => {
+        console.log("------------");
+        this.autoNumberHeadings(editor)
+      },
+    });
 
-interface MyPluginSettings {
-	mySetting: string;
-}
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+  }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  // 自动为标题添加序号的函数
+  autoNumberHeadings(editor: Editor) {
+    // 获取 Markdown 编辑器实例
+  
+	  console.log("1111111111");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    // 获取当前 Markdown 内容
+    const markdownContent = editor.getValue();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    // 按行分割 Markdown 内容
+    const lines = markdownContent.split("\n");
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    // 记录各级标题计数
+    let hCount: number[] = [0, 0, 0];
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    // 遍历每行内容
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      console.log(line);
+      // 判断是否为标题行
+      if (line.startsWith("#")) {
+        // 获取标题级别
+        const level = line.indexOf(" ");
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        // 根据标题级别更新计数并为标题添加序号
+        lines[i] = this.replaceTitle(hCount, level, line);
+        // switch (level) {
+        //   case 1:
+        //     // lines[i] = `# ${h1Count}.${line.substring(1)}`;
+        //     lines[i] = this.replaceTitle(hCount, level, line);
+        //     break;
+        //   case 2:
+          
+        //     // lines[i] = `## ${h1Count}.${h2Count}.${line.substring(2)}`;
+        //     lines[i] = this.replaceTitle(hCount, level, line);
+        //     break;
+        //   case 3:
+            
+        //     // lines[i] = `### ${h1Count}.${h2Count}.${h3Count}. ${line.substring(3)}`;
+        //     break;
+        // }
+      }
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    // 更新 Markdown 编辑器内容
+    editor.setValue(lines.join("\n"));
+  }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
 
-	onunload() {
 
-	}
+  replaceTitle( hCount: number[], level: number, title: string ) {
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+    let regex2: RegExp = /^\d+\.\d+\.$/;
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+    const nums: string[] = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    switch (level){
+      case 1:
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        hCount[0]++;
+        hCount[1] = 0;
+        hCount[2] = 0;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+        
+        
+        `# ${hCount[0]}.${title.substring(1)}`;
+        break;
 
-	display(): void {
-		const {containerEl} = this;
+      case 2:
+        hCount[1]++;
+        hCount[2] = 0;
+        title.replace("1.", "(一).")
 
-		containerEl.empty();
+        break;
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+      case 3:
+        hCount[2]++;
+        break;
+
+    }
+
+    return title;
+
+
+  }
+
+
 }
